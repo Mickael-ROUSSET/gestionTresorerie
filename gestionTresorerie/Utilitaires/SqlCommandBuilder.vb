f3ac873c32d46c5cf43831ce08d56f21a4d9b019
ROUSSET <mickael_rousset@hotmail.com>
2026-04-23 22:11:47 +0200

    fix(data): restore legacy CreateSqlCommand for backward compatibility

--- gestionTresorerie/Utilitaires/SqlCommandBuilder.vb
+++ gestionTresorerie/Utilitaires/SqlCommandBuilder.vb
@@ -1,62 +1,82 @@
 ﻿Imports System.Data.SqlClient
-Imports ADODB
 
 Public Class SqlCommandBuilder
-    ' Constructeur privé pour empêcher l'instanciation directe
+
     Private Sub New()
     End Sub
 
-    ' Méthode pour créer et renvoyer un SqlCommand
-    Public Shared Function CreateSqlCommand(sBase As String, query As String, Optional parameters As Dictionary(Of String, Object) = Nothing) As SqlCommand
-        Try
-            ' Associer la connexion à la commande
-            Dim command As New SqlCommand(LectureProprietes.GetVariable(query)) With {
-                .Connection = ConnexionDB.GetInstance(sBase).getConnexion(sBase)
-            }
-            ' Ajouter les paramètres à la commande si fournis
-            If parameters IsNot Nothing Then
-                For Each param In parameters
-                    command.Parameters.AddWithValue(param.Key, param.Value)
-                Next
-            End If
-            Logger.INFO($"Création de la commande : {command.CommandText }")
+    Private Shared Function BuildExecutor(sBase As String) As ISqlExecutor
+        Dim connectionString As String = ConnexionDB.GetInstance(sBase).GetConnexion(sBase).ConnectionString
+        Dim factory As New AgumaaaConnectionFactory(connectionString)
+        Dim provider As ISqlTextProvider = New LegacySqlTextProvider()
 
-            Return command
-        Catch ex As SqlException
-            Logger.ERR($"Erreur SQL lors de la création de la commande : {ex.Message}")
-            Throw
-        Catch ex As Exception
-            Logger.ERR($"Erreur inattendue lors de la création de la commande : {ex.Message}")
-            Throw
-        End Try
-    End Function    ''' <summary>
-    ''' Exécute une requête SQL paramétrée et retourne les résultats sous forme d'une liste d'objets typés.
-    ''' </summary>
-    ''' <typeparam name="T">TypeMouvement métier cible (ex : Tiers, Categorie...)</typeparam>
-    ''' <param name="nomRequete">Nom ou texte de la requête SQL</param>
-    ''' <param name="parametres">Dictionnaire de paramètres nommés</param>
-    ''' <returns>Liste d'objets de type T</returns>
-    ' 🔹 Exécute une requête et renvoie un DataTable
-    Public Shared Function ExecuteDataTable(sBase As String, nomRequete As String, Optional params As Dictionary(Of String, Object) = Nothing) As DataTable
-        Dim dt As New DataTable
-        Dim conn As SqlConnection = ConnexionDB.GetInstance(sBase).getConnexion(sBase)
-        Using cmd = CreateSqlCommand(sBase, nomRequete, params)
-            cmd.Connection = conn
-            Using da As New SqlDataAdapter(cmd)
-                da.Fill(dt)
+        Return New SqlExecutor(factory, provider)
+    End Function
+
+    Public Shared Function ExecuteDataTable(sBase As String,
+                                            nomRequete As String,
+                                            Optional params As Dictionary(Of String, Object) = Nothing) As DataTable
+        Dim dt As New DataTable()
+        Dim sqlTextProvider As ISqlTextProvider = New LegacySqlTextProvider()
+        Dim sql As String = sqlTextProvider.GetSql(nomRequete)
+
+        Dim connectionString As String = ConnexionDB.GetInstance(sBase).GetConnexion(sBase).ConnectionString
+
+        Using conn As New SqlConnection(connectionString)
+            Using cmd As New SqlCommand(sql, conn)
+                AddDictionaryParameters(cmd, params)
+
+                Using da As New SqlDataAdapter(cmd)
+                    da.Fill(dt)
+                End Using
             End Using
         End Using
+
         Return dt
     End Function
 
-    ' 🔹 Exécute une requête et renvoie une liste d'entités typées
-    Public Shared Function GetEntities(Of T As {BaseDataRow, New})(sBase As String, nomRequete As String, Optional params As Dictionary(Of String, Object) = Nothing) As List(Of T)
+    Public Shared Function GetEntities(Of T As {BaseDataRow, New})(sBase As String,
+                                                                   nomRequete As String,
+                                                                   Optional params As Dictionary(Of String, Object) = Nothing) As List(Of T)
         Try
             Dim dt = ExecuteDataTable(sBase, nomRequete, params)
             Return DataRowUtils.FromDataTableGeneric(Of T)(dt)
         Catch ex As Exception
-            Logger.ERR($"Erreur lors du chargement des entités {GetType(T).Name} depuis {nomRequete} : {ex.Message}")
-            Return New List(Of T)
+            Logger.ERR(String.Format("Erreur lors du chargement des entités {0} depuis {1} : {2}",
+                                     GetType(T).Name,
+                                     nomRequete,
+                                     ex.Message))
+            Return New List(Of T)()
         End Try
+    End Function
+
+    Private Shared Sub AddDictionaryParameters(cmd As SqlCommand, params As Dictionary(Of String, Object))
+        If params Is Nothing Then
+            Exit Sub
+        End If
+
+        For Each param In params
+            cmd.Parameters.AddWithValue(param.Key, If(param.Value, DBNull.Value))
+        Next
+    End Sub
+    Public Shared Function CreateSqlCommand(queryName As String,
+                                        sBase As String,
+                                        Optional params As Dictionary(Of String, Object) = Nothing) As SqlCommand
+
+        Dim sqlTextProvider As ISqlTextProvider = New LegacySqlTextProvider()
+        Dim sql As String = sqlTextProvider.GetSql(queryName)
+
+        Dim connectionString As String = ConnexionDB.GetInstance(sBase).GetConnexion(sBase).ConnectionString
+        Dim conn As New SqlConnection(connectionString)
+
+        Dim cmd As New SqlCommand(sql, conn)
+
+        If params IsNot Nothing Then
+            For Each p In params
+                cmd.Parameters.AddWithValue(p.Key, If(p.Value, DBNull.Value))
+            Next
+        End If
+
+        Return cmd
     End Function
 End Class
